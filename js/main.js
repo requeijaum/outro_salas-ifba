eab9fdd6a6444be1d01a9b53e7f82cdcfcac0e9d
Rafael Requião <rafaelfrequiao@gmail.com>
2019-12-21 22:38:30 -0300

    Iniciando listagem via BS v4

--- js/main.js
+++ js/main.js
@@ -1,12 +1,19 @@
 // vamos gerar a tabela...
 
+/*
 var list = [
     { "col_1": "val_11", "col_3": "val_13" },
     { "col_2": "val_22", "col_3": "val_23" },
     { "col_1": "val_31", "col_3": "val_33" }
 ];
+*/
 
-function constructTable(selector) {
+function insereMsgAlerta(alert, msg){
+    let txt = alert.replace('</div>', msg + '</div>');
+    return txt ; 
+}
+
+function constructTable(selector, list) {
 
     console.log("constructTable(): initializing...");
     // Getting the all column names 
@@ -27,6 +34,26 @@ function constructTable(selector) {
         // Adding each row to the table 
         $(selector).append(row);
     }
+}
+
+function constructList(nomeAula, nomeSala, nomeArea, dados){
+    
+    // construir a porra toda pq sim... sem filtrar
+
+    if ((nomeAula == "") && (nomeSala == "") && (nomeArea == "")) {
+        
+        for (sala in dados.dados) { // in ou of ?
+            // precisa ser numérico !!!
+            if (isFinite(sala)) {
+                obj = dados.dados[sala]
+                console.log("constructList(): iterando sala #" + sala + "...")
+                console.log(obj);
+                htmlToAppend = '<li id="sala' + sala + '" class="list-group-item">' + obj["nomeSala"] + '<br> <small>' + obj["nomeArea"] + '</small></li>';
+                $("#the_list").append(htmlToAppend);
+            }
+        }
+    } 
+
 }
 
 function Headers(list, selector) {
@@ -65,16 +92,27 @@ $(document).ready(function () {
     hoje = ""; hoje = hoje.concat(ano).concat(mes).concat(dia);
     dataEspecificada = hoje;  // por enquanto...
 
-    var capturedData;
+    var alertErro                   = '<div id="error" class="alert alert-danger" role="alert">' + '</div>';  // usar innerHTML pra setar algo no div :^)
+    var msgErro_Requisicao          = "Falha ao obter dados de horários!!!";
+
+    var capturedData = null;
 
     $.getJSON("dados/capturas/" + dataEspecificada + ".json", function (json) {
-        console.log(json);
+        //console.log(json);
+        capturedData = json;
+    })
+    .fail(function(){
+        console.log(msgErro_Requisicao) ; $('main').append(insereMsgAlerta(alertErro, msgErro_Requisicao)) 
+    })
+    .done(function(){
+        console.log(capturedData); // constructTable('#table', capturedData) ;
+        constructList("", "", "", capturedData)  // constructList(nomeAula, nomeSala, nomeArea, dados) --> 'nomeProfessor' incluso em nomeAula
     });
+        
+
 
     // Agora vamos criar uma lista a partir do que já temos?
     // https://www.w3schools.com/howto/howto_js_filter_lists.asp
 
-    // vamos gerar a tabela?
-    constructTable('#table');
 
 });
