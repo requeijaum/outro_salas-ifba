bef9f84a9835d81e346b01ea30b1d04773a4fe0b
Rafael Felix Silva Requião <rafaelfrequiao@gmail.com>
2019-12-23 11:22:05 -0300

    verificar escolherModoBusca() com a Promise voltando...

--- js/main.js
+++ js/main.js
@@ -6,13 +6,6 @@ var list = [
     { "col_2": "val_22", "col_3": "val_23" },
     { "col_1": "val_31", "col_3": "val_33" }
 ];
-*/
-
-function insereMsgAlerta(alert, msg){
-    let txt = alert.replace('</div>', msg + '</div>');
-    return txt ; 
-}
-
 function constructTable(selector, list) {
 
     console.log("constructTable(): initializing...");
@@ -36,12 +29,24 @@ function constructTable(selector, list) {
     }
 }
 
+
+*/
+
+function insereMsgAlerta(alert, msg){
+    let txt = alert.replace('</div>', msg + '</div>');
+    return txt ; 
+}
+
 function constructList(nomeAula, nomeSala, nomeArea, dados){
     
+    // limpar tudo, né?
+    $('#the_list').html("");
+
     // construir a porra toda pq sim... sem filtrar
 
     if ((nomeAula == "") && (nomeSala == "") && (nomeArea == "")) {
 
+        // mostrar tudo !!!
         num_salas = Object.keys(dados.dados).length - 1 ;
         console.log('constructList(): mostrando todas as ' + num_salas + ' salas!'); 
         //  ^^^ removi a chave "id"
@@ -82,10 +87,59 @@ function constructList(nomeAula, nomeSala, nomeArea, dados){
         }
     } 
 
-}
+    if ((nomeAula != "") && (nomeSala == "") && (nomeArea == "")) {
 
-function dispararBusca(){
-    console.log("busca disparada!");
+        // procurar pelo nome da aula
+        num_salas = Object.keys(dados.dados).length - 1 ;
+        console.log('constructList(): mostrando todas as ' + num_salas + ' salas!'); 
+        //  ^^^ removi a chave "id"
+
+        for (sala in dados.dados) { // in ou of ?
+            // precisa ser numérico !!!
+            if (isFinite(sala) && (nomeAula in dados.dados[sala].horarios)) {
+                obj = dados.dados[sala]
+                // console.log("constructList(): iterando sala #" + sala + "...")
+                // console.log(obj);
+
+                // preciso de alguma condicional pra mostrar somente as salas com dados.dados[sala].horarios == nomeAula
+                for (horario in dados.dados[sala].horarios) {
+                    console.log(horario);
+                    if (horario == nomeSala) {
+                        console.log("horario == nomeSala!!!");
+
+                        htmlToAppend = '<li id="sala' + sala + 
+                        '" class="list-group-item"> <div class="row"><div class="sala_info col-*-6 push-left"> ' 
+                        + obj["nomeSala"] + '<br> <small>' + obj["nomeArea"] + '</small>\
+                        </div>\
+                        <div class="sala_info_extra col-*-6 push-right">\
+                        <ul class="lista_aulas"></ul>\
+                        </div></div>\
+                        </li>';
+                        
+                        $("#the_list").append(htmlToAppend);
+                        
+                        // esconder sala caso não tenha horarios?
+                        if (Object.keys(obj.horarios).length == 0){
+                            console.log("A sala #" + sala + " - " + obj["nomeSala"] + " - não tem horários...");
+                            $("#sala"+sala).css("display","none");
+                        
+                        }
+
+                        // $("#sala"+sala+" .lista_aulas").append("<li>Teste 123</li>");
+                        for (horario in obj.horarios) {
+                            $("#sala"+sala+" .lista_aulas").append("<li><b>" + horario + "</b> - " 
+                            + obj.horarios[horario] + "</li>");
+
+                        }
+                    }
+                }
+                
+            }
+        }
+
+    
+    }
+
 }
 
 function Headers(list, selector) {
@@ -110,6 +164,10 @@ function Headers(list, selector) {
     return columns;
 }
 
+// instanciar, né?
+
+var capturedData = null;
+
 $(document).ready(function () {
     console.log("Preparado!");
     // qual a data de hoje?
@@ -128,7 +186,6 @@ $(document).ready(function () {
     var alertErro                   = '<div id="error" class="alert alert-danger" role="alert">' + '</div>';  // usar innerHTML pra setar algo no div :^)
     var msgErro_Requisicao          = "Falha ao obter dados de horários!!!";
 
-    var capturedData = null;
 
     // terei que repetir isso a cada especificacao de data... 
 
@@ -158,6 +215,7 @@ $(document).ready(function () {
 // funções auxiliares...
 
 // prevenir que Enter ou qualquer outra coisa atualize a página
+/*
 var preventSubmit = function(event) {
     if(event.keyCode == 13) {
         console.log("caught ya!");
@@ -166,24 +224,91 @@ var preventSubmit = function(event) {
         return false;
     }
 }
-$("#search_bar").keypress(preventSubmit);
-$("#search_bar").keydown(preventSubmit);
-$("#search_bar").keyup(preventSubmit);
+
+$("#search_bar :input").keypress(preventSubmit);
+$("#search_bar :input").keydown(preventSubmit);
+$("#search_bar :input").keyup(preventSubmit);
+
+$('#search_input :input').keydown(function (e) {
+    if (e.keyCode == 13) {
+        e.preventDefault();
+        return false;
+    }
+});
+
+*/
 
 
 // vamos verificar a dropdown list de opções?
 
 var opcoes = {
-    "nomeAula" : false,
+    "nomeAula" : true,
     "nomeSala" : false,
     "nomeArea" : false,
 };
 
-function verificarDropdown(data){
+function verificarDropdown(data, opcoes){
     //console.log(data);
+    console.log(opcoes);
+
     console.log("verificarDropdown(): hmm...");
+    
+    if(opcoes.nomeAula){
+        $('#nomeAula_drop').addClass('active');
+    } else {$('#nomeAula_drop').removeClass('active');}
+    
+    if(opcoes.nomeSala){
+        $('#nomeSala_drop').addClass('active');
+    } else {$('#nomeSala_drop').removeClass('active');}
+    
+    if(opcoes.nomeArea){
+        $('#nomeArea_drop').addClass('active');
+    } else {$('#nomeArea_drop').removeClass('active');}
 
 }
+
+
+function escolheModoBusca(data, opcoes) {   // tá dando alguma merda com Promise indo e voltando como fullfiled...
+    
+    var opcoes = opcoes ;
+
+    console.log("seguem as opcoes antes da operacao...");
+    console.log(opcoes); // mostra o final da operacao - pq ta aparecendo uma promise?
+            
+
+    nomeOpcao = $(data).attr('name');
+    console.log("opcao escolhida: " + nomeOpcao);
+    
+    async function resetarOpcoes(opcoes){   // reseta tudo pra falso
+        novasOpcoes = opcoes;
+        novasOpcoes.nomeAula = false; novasOpcoes.nomeSala = false; novasOpcoes.nomeArea = false; 
+        return novasOpcoes; 
+    }
+    
+    async function setarOpcao(nomeOpcao, opcoes){  
+        
+        if(nomeOpcao){ // lembre-se: JS roda tudo em paralelo...
+            
+            opcoesDefinitivas = await resetarOpcoes(opcoes); // depois de resetar...
+
+            opcoesDefinitivas[nomeOpcao] = true; // joga a opcao correta pra true, de forma exclusiva
+            console.log("seguem as opcoes depois da operacao...");
+            console.log(opcoesDefinitivas); // mostra o final da operacao
+            
+            //return opcoesDefinitivas;
+            
+            if(typeof(opcoesDefinitivas) == typeof(Promise))  return opcoesDefinitivas.value ;
+            if(typeof(opcoesDefinitivas) == typeof(Object))   return opcoesDefinitivas ;
+
+        }
+    
+    }
+
+    return setarOpcao(nomeOpcao, opcoes); // retorna opcoesDefinitivas via async ;^)
+
+
+}
+
 
 // vamos instanciar o Tempus Dominus para disparar sempre que clicar na parte da escolha de data
 function setarDataBusca(data){
@@ -198,3 +323,27 @@ $(function(){
     $('#datetimepicker1').datetimepicker();
 
 });
+
+function buscar(data, allData){
+
+    //console.log(data.value);
+    // avaliar qual a opção setada...
+
+    if( (opcoes.nomeAula) && !(opcoes.nomeSala) && !(opcoes.nomeArea) ) {
+        console.log("buscar('" + data.value + "'): construindo lista por nomeAula");
+        constructList(data.value, "", "", allData);  // function constructList(nomeAula, nomeSala, nomeArea, dados){ ...
+    }
+
+    if( !(opcoes.nomeAula) && (opcoes.nomeSala) && !(opcoes.nomeArea) ) {
+        console.log("buscar(): construindo lista por nomeSala");
+        constructList("", data.value, "", allData);  // function constructList(nomeAula, nomeSala, nomeArea, dados){ ...
+    }
+
+    if( !(opcoes.nomeAula) && !(opcoes.nomeSala) && (opcoes.nomeArea) ) {
+        console.log("buscar(): construindo lista por nomeArea");
+        constructList("", "", data.value, allData);  // function constructList(nomeAula, nomeSala, nomeArea, dados){ ...
+    }
+
+
+
+}
